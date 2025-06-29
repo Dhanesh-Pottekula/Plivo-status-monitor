ad98b86165d62247954437783e1dcae962d5e71f
dhanesh <dhanesh@zydelo.com>
2025-06-30 02:58:15 +0530

    console removed

--- plivo-frontend/src/_helpers/BaseRoute.tsx
+++ plivo-frontend/src/_helpers/BaseRoute.tsx
@@ -3,18 +3,22 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../_contexts/AuthContext";
 import { appRoutes } from "../config/appRoutes";
 
+
 const BaseUrlComponent = () => {
-  const { user } = useAuth();
+  const { user,isLoading } = useAuth();
+  if(isLoading){
+    return 
+  }
   if (!user?.id) {
     return <Navigate replace to={appRoutes.login} />;
   }
+
   if (user?.role === "admin") {
     return <Navigate replace to={appRoutes.team_members} />;
   }else if (user?.role === "team" && user?.organization?.id) {
-    return <Navigate replace to={appRoutes.services.replace(":orgId",user?.organization?.id||"")} />;
-  }else{
-    return <Navigate replace to={appRoutes.organizations_list} />;
+    return <Navigate replace to={appRoutes.services.replace(":org_id",user?.organization?.id||"")} />;
   }
+
 
 };
 
