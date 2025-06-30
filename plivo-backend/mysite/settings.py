b4aef2839fbe1ed9f62ddb329df59311f929fc5b
dhanesh <dhanesh@zydelo.com>
2025-06-30 10:42:38 +0530

    added frontend url in settngs

--- plivo-backend/mysite/settings.py
+++ plivo-backend/mysite/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite dev server
     "http://127.0.0.1:5173",
-    "https://plivo-frontend.onrender.com"
+    "https://plivo-status-monitor-frontend.onrender.com"
 ]
 ALLOWED_HOSTS = [
     'plivo-status-monitor.onrender.com',
