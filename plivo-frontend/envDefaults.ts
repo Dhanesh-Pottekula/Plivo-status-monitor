4038f4f088c7f9fc8c63457a45ce0a76713cc77a
dhanesh <dhanesh@zydelo.com>
2025-06-30 10:19:05 +0530

    added backend url in frontend

--- plivo-frontend/envDefaults.ts
+++ plivo-frontend/envDefaults.ts
@@ -1,3 +1,6 @@
 export const envDefaults = {
-    nodeApiUrl: import.meta.env.VITE_NODE_API_URL || 'http://localhost:8000',
+    nodeApiUrl: import.meta.env.VITE_NODE_API_URL || 'https://plivo-status-monitor.onrender.com',
+    wsUrl: import.meta.env.VITE_WS_URL || 'wss://plivo-status-monitor.onrender.com',
+    // nodeApiUrl: import.meta.env.VITE_NODE_API_URL || 'http://localhost:8000',
+    // wsUrl: import.meta.env.VITE_WS_URL || 'ws://localhost:8765/',
 }
--- plivo-frontend/src/lib/ws_socket.ts
+++ plivo-frontend/src/lib/ws_socket.ts
@@ -1,3 +1,5 @@
+import { envDefaults } from "envDefaults";
+
 // wsManager.ts
 type Listener = (data: any) => void;
 
@@ -13,7 +15,8 @@ class WebSocketManager {
 
     if (!this.connectionPromise) {
       this.connectionPromise = new Promise((resolve, reject) => {
-        this.socket = new WebSocket("ws://localhost:8765/");
+        this.socket = new WebSocket(envDefaults.wsUrl);
+
 
         this.socket.onopen = () => {
           console.log("✅ WebSocket connected");
